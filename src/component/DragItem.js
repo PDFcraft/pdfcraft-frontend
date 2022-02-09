3088741a60ea1487c59c450755237c8401c1a49b
chobobdev <chobobdev@naver.com>
2022-02-09 18:22:39 +0900

    Feat: Send item list

--- src/component/DragItem.js
+++ src/component/DragItem.js
@@ -1,14 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 // a little function to help us with reordering the result
-const reorder = (list, startIndex, endIndex) => {
-    const result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
-
-    return result;
-};
 
 const grid = 8;
 
@@ -32,42 +25,8 @@ const getListStyle = (isDraggingOver) => ({
     overflow: "auto"
 });
 
-const DragItem = () => {
-    const [items, setItems] = useState([
-        {
-            id: "item-1",
-            content: "item 1"
-        },
-        {
-            id: "item-2",
-            content: "item 2"
-        },
-        {
-            id: "item-3",
-            content: "item 3"
-        },
-        {
-            id: "item-4",
-            content: "item 4"
-        },
-        {
-            id: "item-5",
-            content: "item 5"
-        }
-    ]);
-
-    const onDragEnd = (result) => {
-        if (result.destination) {
-            const newItems = reorder(
-                items,
-                result.source.index,
-                result.destination.index
-            );
-
-            setItems(newItems);
-        }
-    };
-
+const DragItem = (files, onDragEnd, items) => {
+    console.log(files)
     return (
         <DragDropContext onDragEnd={onDragEnd}>
             <Droppable droppableId="droppable" direction="horizontal">
@@ -77,7 +36,7 @@ const DragItem = () => {
                         style={getListStyle(snapshot.isDraggingOver)}
                         {...provided.droppableProps}
                     >
-                        {items.map((item, index) => (
+                        {items?.map((item, index) => (
                             <Draggable key={item.id} draggableId={item.id} index={index}>
                                 {(provided, snapshot) => (
                                     <div
--- src/component/Merge.js
+++ src/component/Merge.js
@@ -1,18 +1,71 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DragItem from "./DragItem";
 import DropzoneComponent from "./DropzoneComponent";
 
+const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+};
+
 const Merge = () => {
-    const [fileNames, setFileNames] = useState([]);
+
+    const [items, setItems] = useState([
+        {
+            id: "item-1",
+            content: "item 1"
+        },
+        {
+            id: "item-2",
+            content: "item 2"
+        },
+        {
+            id: "item-3",
+            content: "item 3"
+        },
+        {
+            id: "item-4",
+            content: "item 4"
+        },
+        {
+            id: "item-5",
+            content: "item 5"
+        }
+    ]);
+    const onDragEnd = (result) => {
+        if (result.destination) {
+            const newItems = reorder(
+                items,
+                result.source.index,
+                result.destination.index
+            );
+
+            setItems(newItems);
+        }
+    };
+
+
+    const [files, setFiles] = useState([]);
     const handleDrop = acceptedFiles => {
-        setFileNames(acceptedFiles.map(file => file.name));
-        console.log(acceptedFiles);
+        setFiles(acceptedFiles);
     }
+
+    useEffect((items) => {
+        console.log("updated")
+        setItems(files.map((file, index) => ({
+            id: `${index}`,
+            content: `${file.name}`
+        })));
+
+        console.log(items)
+    }, [files])
     return (
         <div>
             <h1>Merge</h1>
-            <DropzoneComponent fileNames={fileNames} handleDrop={handleDrop}></DropzoneComponent>
-            <DragItem fileNames={fileNames} />
+            <DropzoneComponent files={files} handleDrop={handleDrop}></DropzoneComponent>
+            <DragItem files={files} onDragEnd={onDragEnd} items={items} />
         </div>
     );
 };

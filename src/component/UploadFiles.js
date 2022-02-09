d06101643271093bbe097b6b9a4d9ce0792fa0ef
chobobdev <chobobdev@naver.com>
2022-02-10 02:19:32 +0900

    Feat : console.log() response for test

--- src/component/UploadFiles.js
+++ src/component/UploadFiles.js
@@ -8,7 +8,6 @@ export const UploadFiles = (files, sortedFiles) => {
         for (var j in files) {
             if (files[j].name === Object.values(sortedFiles[i])[1]) {
                 formData.append('files', files[j]);
-                console.log(files[j])
             }
         }
     }
@@ -18,7 +17,7 @@ export const UploadFiles = (files, sortedFiles) => {
         },
     }).then((resp) => {
         if (resp.status === 200) {
-            console.log('File uploaded');
+            console.log(resp.data);
         }
     });
 };

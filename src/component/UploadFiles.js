5cc5411eadfc498e20e6af458f44dfa158dbf9f4
chobobdev <chobobdev@naver.com>
2022-02-10 02:03:35 +0900

    Refactor: refactored reordering list function

--- src/component/UploadFiles.js
+++ src/component/UploadFiles.js
@@ -3,22 +3,14 @@ import Axios from 'axios';
 
 
 export const UploadFiles = (files, sortedFiles) => {
-    let sortedFileLists = []
-    let sortedFileNames = []
+    const formData = new FormData();
     for (var i in sortedFiles) {
-        let fileName = sortedFileNames.push(Object.values(sortedFiles[i])[1])
-    }
-    for (var j in sortedFileNames) {
-        for (var k in files) {
-            if (files[k].name === sortedFileNames[j]) {
-                let addInOrder = sortedFileLists.push(files[k])
+        for (var j in files) {
+            if (files[j].name === Object.values(sortedFiles[i])[1]) {
+                formData.append('files', files[j]);
+                console.log(files[j])
             }
         }
-    }
-    console.log(sortedFileLists)
-    const formData = new FormData();
-    for (var l in files) {
-        formData.append('files', files[l]);
     }
     Axios.post('http://localhost:8080/merge', formData, {
         headers: {

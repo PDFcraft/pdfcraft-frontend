f7301e6accdffb23c7f8c3270bc2fd67962b6b3b
chobobdev <chobobdev@naver.com>
2022-02-12 12:46:44 +0900

    Feat : Defer recoil for restructuring

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import RecoilRoot from 'recoil';
 import Merge from "./Routes/Merge";
 import Home from "./Routes/Home"
 import Split from "./Routes/Split";
 
 function App() {
   return (
-    <RecoilRoot>
-      <Router>
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/merge" exact element={<Merge />} />
-          <Route path="/split" exact element={<Split />} />
-        </Routes>
-      </Router>
-    </RecoilRoot>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/merge" exact element={<Merge />} />
+        <Route path="/split" exact element={<Split />} />
+      </Routes>
+    </Router>
   );
 };
 
